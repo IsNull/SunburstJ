8f169ee19527a97ba62e9c42216a9c9ebc324451
IsNull <isnull@securityvision.ch>
2014-04-21 12:10:22 +0200

    Implemented static drawing of Donut (semi ring)

--- sunburst.control/src/main/java/controls/sunburst/DonutUnit.java
+++ sunburst.control/src/main/java/controls/sunburst/DonutUnit.java
@@ -10,72 +10,74 @@ public class DonutUnit extends Path {
 
     public DonutUnit(){
 
+        drawDonutUnit(
+                200,      // Center X
+                200,      // Center Y
+                -90,    // Start angle
+                90,     // End angle
+                40,     // Inner Radius
+                30);   // Ring-Width
 
-        drawSemiRing(120, 120, 100, 50);
 
         this.setFill(Color.LIGHTGREEN);
         this.setStroke(Color.DARKGREEN);
         this.setFillRule(FillRule.EVEN_ODD);
-
-    }
-
-    private void drawDonutUnit(double radius, double width, double angle){
-
-        double innerRadius = radius;
-        double outerRadius = radius + width;
-
-
-        MoveTo moveTo = new MoveTo();
-        moveTo.setX(0);
-        moveTo.setY(0);
-        this.getElements().add(moveTo);
-
-        /*
-        ArcTo arcToInner = new ArcTo();
-        arcToInner.setX(centerX - innerRadius);
-        arcToInner.setY(centerY);
-        arcToInner.setRadiusX(innerRadius);
-        arcToInner.setRadiusY(innerRadius);
-        */
-
     }
 
 
-    private void drawSemiRing(double centerX, double centerY, double radius, double innerRadius) {
+    /**
+     * Creates a semi ring (Donut) path
+     *
+     * @param centerX Center Point X Coordinate
+     * @param centerY Center Point Y Coordinate
+     * @param degreeStart Start angle in degree, may be negative.
+     * @param degreeEnd End angle in degree, may be negative.
+     * @param innerRadius Inner radius of the Donut
+     * @param ringWidth Width of the Donut ring.
+     */
+    private void drawDonutUnit(double centerX, double centerY, double degreeStart, double degreeEnd, double innerRadius, double ringWidth) {
+        double angleAlpha = degreeStart * (Math.PI / 180);
+        double angleAlphaNext = degreeEnd * (Math.PI / 180);
 
-        MoveTo moveTo = new MoveTo();
-        moveTo.setX(centerX + innerRadius);
-        moveTo.setY(centerY);
+        double outerRadius = innerRadius + ringWidth;
 
-        ArcTo arcToInner = new ArcTo();
-        arcToInner.setX(centerX - innerRadius);
-        arcToInner.setY(centerY);
-        arcToInner.setRadiusX(innerRadius);
-        arcToInner.setRadiusY(innerRadius);
+        //Point 1
+        double pointX1 = centerX + innerRadius * Math.sin(angleAlpha);
+        double pointY1 = centerY - innerRadius * Math.cos(angleAlpha);
 
-        MoveTo moveTo2 = new MoveTo();
-        moveTo2.setX(centerX + innerRadius);
-        moveTo2.setY(centerY);
+        //Point 2
+        double pointX2 = centerX + outerRadius * Math.sin(angleAlpha);
+        double pointY2 = centerY - outerRadius * Math.cos(angleAlpha);
 
-        HLineTo hLineToRightLeg = new HLineTo();
-        hLineToRightLeg.setX(centerX + radius);
+        //Point 3
+        double pointX3 = centerX + outerRadius * Math.sin(angleAlphaNext);
+        double pointY3 = centerY - outerRadius * Math.cos(angleAlphaNext);
 
-        ArcTo arcTo = new ArcTo();
-        arcTo.setX(centerX - radius);
-        arcTo.setY(centerY);
-        arcTo.setRadiusX(radius);
-        arcTo.setRadiusY(radius);
+        //Point 4
+        double pointX4 = centerX + innerRadius * Math.sin(angleAlphaNext);
+        double pointY4 = centerY - innerRadius * Math.cos(angleAlphaNext);
 
-        HLineTo hLineToLeftLeg = new HLineTo();
-        hLineToLeftLeg.setX(centerX - innerRadius);
 
-        this.getElements().add(moveTo);
-        this.getElements().add(arcToInner);
-        this.getElements().add(moveTo2);
-        this.getElements().add(hLineToRightLeg);
-        this.getElements().add(arcTo);
-        this.getElements().add(hLineToLeftLeg);
+        this.getElements().add(new MoveTo(pointX1, pointY1)); // Move to Point 1
+        this.getElements().add(new LineTo(pointX2, pointY2)); // Draw a Line to Point 2
+
+        ArcTo arcOuter = new ArcTo();                         // Draw an Arc to Point 3
+        arcOuter.setRadiusX(outerRadius);
+        arcOuter.setRadiusY(outerRadius);
+        arcOuter.setX(pointX3);
+        arcOuter.setY(pointY3);
+        arcOuter.setSweepFlag(true);
+        this.getElements().add(arcOuter);
+
+        this.getElements().add(new LineTo(pointX4, pointY4)); // Draw a Line to Point 4
+
+        ArcTo arcInner = new ArcTo();                         // Draw an Arc to Point 1
+        arcInner.setRadiusX(innerRadius);
+        arcInner.setRadiusY(innerRadius);
+        arcInner.setX(pointX1);
+        arcInner.setY(pointY1);
+        arcInner.setSweepFlag(false);
+        this.getElements().add(arcInner);
     }
-
 
 }
